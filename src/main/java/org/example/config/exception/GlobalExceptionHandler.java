ed448bb9bec76f7ea1dfc9279f69b690ce9e5954
linzhaoming <linzhaoming@ava.com.cn>
2020-10-23 16:07:01 +0800

    feat: add orm framework

---
+++ src/main/java/org/example/config/exception/GlobalExceptionHandler.java
@@ -0,0 +1,11 @@
+package org.example.config.exception;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Order(value = Ordered.HIGHEST_PRECEDENCE)
+public class GlobalExceptionHandler {
+
+}
---
+++ src/main/java/org/example/generator/CodeGenerator.java
@@ -0,0 +1,14 @@
+package org.example.generator;
+
+
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+
+public class CodeGenerator {
+
+    private static final AutoGenerator mpg  = new AutoGenerator();
+
+    public static void main(String[] args) {
+
+    }
+
+}
