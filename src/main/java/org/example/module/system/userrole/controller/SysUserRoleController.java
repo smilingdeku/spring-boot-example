bf9aa944886de089b4b1ea6a858950e43b0f33a8
linzhaoming <linzhaoming@ava.com.cn>
2020-10-26 10:16:14 +0800

    feat: add user role

---
+++ src/main/java/org/example/module/system/userrole/controller/SysUserRoleController.java
@@ -0,0 +1,20 @@
+package org.example.module.system.userrole.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 系统用户角色关联 前端控制器
+ * </p>
+ *
+ * @author linzhaoming
+ * @since 2020-10-26
+ */
+@RestController
+@RequestMapping("/system/user-role")
+public class SysUserRoleController {
+
+}
---
+++ src/main/java/org/example/module/system/userrole/service/impl/SysUserRoleServiceImpl.java
@@ -0,0 +1,20 @@
+package org.example.module.system.userrole.service.impl;
+
+import org.example.module.system.userrole.entity.SysUserRole;
+import org.example.module.system.userrole.mapper.SysUserRoleMapper;
+import org.example.module.system.userrole.service.ISysUserRoleService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 系统用户角色关联 服务实现类
+ * </p>
+ *
+ * @author linzhaoming
+ * @since 2020-10-26
+ */
+@Service
+public class SysUserRoleServiceImpl extends ServiceImpl<SysUserRoleMapper, SysUserRole> implements ISysUserRoleService {
+
+}
