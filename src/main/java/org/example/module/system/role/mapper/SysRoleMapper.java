ac739a8350125ccdfae4295ff73e1e7daa8cc470
linzhaoming <linzhaoming@ava.com.cn>
2020-10-26 10:12:10 +0800

    feat: add role

---
+++ src/main/java/org/example/module/system/role/mapper/SysRoleMapper.java
@@ -0,0 +1,16 @@
+package org.example.module.system.role.mapper;
+
+import org.example.module.system.role.entity.SysRole;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 系统角色 Mapper 接口
+ * </p>
+ *
+ * @author linzhaoming
+ * @since 2020-10-26
+ */
+public interface SysRoleMapper extends BaseMapper<SysRole> {
+
+}
---
+++ src/main/java/org/example/module/system/role/service/ISysRoleService.java
@@ -0,0 +1,16 @@
+package org.example.module.system.role.service;
+
+import org.example.module.system.role.entity.SysRole;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 系统角色 服务类
+ * </p>
+ *
+ * @author linzhaoming
+ * @since 2020-10-26
+ */
+public interface ISysRoleService extends IService<SysRole> {
+
+}
