beb94e06e544cdc7f2a67e8849c2c1c4cb2f2b91
linzhaoming <linzhaoming@ava.com.cn>
2020-11-30 12:03:53 +0800

    docs: 添加 quartz misfire 注释

--- src/main/java/org/example/module/system/schedulejob/enums/ScheduleJobMisfirePolicy.java
+++ src/main/java/org/example/module/system/schedulejob/enums/ScheduleJobMisfirePolicy.java
@@ -1,14 +1,32 @@
 package org.example.module.system.schedulejob.enums;
 
 /**
+ * 任务触发超时执行策略
+ *
+ * 有如下情况可能导致任务触发超时：
+ * 1）系统重启，在关闭到重启的一段时间，任务无法正常触发执行
+ * 2）任务被暂停的一段时间里，有些任务无法被触发执行
+ * 3）任务不允许并发，在下一次触发时间到达时，上一次执行还没结束
+ *
+ * misfireThreshold 为触发器超时的临界值，默认为 1 minute，若超过这个阈值，则认为超时，否则为不超时
+ *
  * @author linzhaoming
  * @since 2020/11/27
  **/
 public enum ScheduleJobMisfirePolicy {
 
     DEFAULT(0),
+    /**
+     *忽略 misfire 策略，不触发发生 misfire 的任务
+     */
     DO_NOTHING(1),
+    /**
+     * 立刻触发发生 misfire 的任务
+     */
     IGNORE_MISFIRES(2),
+    /**
+     * 立刻触发一次发生 misfire 的任务
+     */
     FIRE_AND_PROCEED(3),
     ;
 
