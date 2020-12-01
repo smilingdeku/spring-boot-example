6d1dd7d422d1e67dee225b0f5dd7d4e9e194cf6f
linzhaoming <linzhaoming@ava.com.cn>
2020-12-01 16:25:43 +0800

    revert: 代码回滚

--- src/main/java/org/example/common/util/BeanCopyUtil.java
+++ src/main/java/org/example/common/util/BeanCopyUtil.java
@@ -1,17 +1,11 @@
 package org.example.common.util;
 
-import com.baomidou.mybatisplus.core.toolkit.StringPool;
 import ma.glasnost.orika.MapperFacade;
-import ma.glasnost.orika.MapperFactory;
 import ma.glasnost.orika.impl.DefaultMapperFactory;
-import ma.glasnost.orika.metadata.ClassMapBuilder;
 import ma.glasnost.orika.metadata.Type;
 import ma.glasnost.orika.metadata.TypeFactory;
 
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author huapeng.huang
@@ -21,7 +15,6 @@ import java.util.concurrent.ConcurrentHashMap;
 public class BeanCopyUtil {
 
     private static MapperFacade MAPPER = new DefaultMapperFactory.Builder().build().getMapperFacade();
-    private static Map<String, MapperFacade> CACHE_MAPPER_MAP = new ConcurrentHashMap<>();
 
     /**
      * 映射实体
@@ -32,19 +25,6 @@ public class BeanCopyUtil {
      */
     public static <S, D> D map(S source, Class<D> destinationClass) {
         return MAPPER.map(source, destinationClass);
-    }
-
-    /**
-     * 映射实体
-     *
-     * @param source           源对象
-     * @param destinationClass 目标类
-     * @param fieldMap         字段映射
-     * @return 目标对象
-     */
-    public static <S, D> D map(S source, Class<D> destinationClass, Map<String, String> fieldMap) {
-        MapperFacade mapper = getMapper(source.getClass(), destinationClass, fieldMap);
-        return mapper.map(source, destinationClass);
     }
 
     /**
@@ -69,20 +49,6 @@ public class BeanCopyUtil {
      */
     public static <S, D> List<D> mapList(Iterable<S> sourceList, Class<S> sourceClass, Class<D> destinationClass) {
         return MAPPER.mapAsList(sourceList, TypeFactory.valueOf(sourceClass), TypeFactory.valueOf(destinationClass));
-    }
-
-    /**
-     * 映射列表
-     *
-     * @param sourceList       源对象列表
-     * @param sourceClass      源对象类
-     * @param destinationClass 目标类
-     * @param fieldMap         字段映射
-     * @return 目标对象列表
-     */
-    public static <S, D> List<D> mapList(Iterable<S> sourceList, Class<S> sourceClass, Class<D> destinationClass, Map<String, String> fieldMap) {
-        MapperFacade mapper = getMapper(sourceClass, destinationClass, fieldMap);
-        return mapper.mapAsList(sourceList, TypeFactory.valueOf(sourceClass), TypeFactory.valueOf(destinationClass));
     }
 
 
@@ -131,29 +97,6 @@ public class BeanCopyUtil {
      */
     public static <E> Type<E> getType(final Class<E> rawType) {
         return TypeFactory.valueOf(rawType);
-    }
-
-    /**
-     * 获取自定义映射
-     *
-     * @param sourceClass      源类
-     * @param destinationClass 目标类
-     * @param fieldMap         字段映射
-     * @return 映射类对象
-     */
-    private static <S, D> MapperFacade getMapper(Class<S> sourceClass, Class<D> destinationClass, Map<String, String> fieldMap) {
-        String mapperKey = String.join(StringPool.DASH,
-                sourceClass.getName(), destinationClass.getName(), String.valueOf(fieldMap.hashCode()));
-        MapperFacade mapper = CACHE_MAPPER_MAP.get(mapperKey);
-        if (Objects.isNull(mapper)) {
-            MapperFactory factory = new DefaultMapperFactory.Builder().build();
-            ClassMapBuilder<S, D> classMapBuilder = factory.classMap(sourceClass, destinationClass);
-            fieldMap.forEach(classMapBuilder::field);
-            classMapBuilder.byDefault().register();
-            mapper = factory.getMapperFacade();
-            CACHE_MAPPER_MAP.put(mapperKey, mapper);
-        }
-        return mapper;
     }
 
 }
