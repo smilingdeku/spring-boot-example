0cb25f893703ef8bb6bddfb5df5806d5981e7dad
cs12110 <cs12110@163.com>
2020-12-10 22:24:47 +0800

    feat: 添加RestTemplate配置和相关Service

---
+++ src/main/java/org/example/config/rpc/RestTemplateConfig.java
@@ -0,0 +1,34 @@
+package org.example.config.rpc;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * RestTemplate 配置类
+ *
+ * @author walle
+ * @version V1.0
+ * @since 2020-12-10 21:48
+ */
+@Configuration
+public class RestTemplateConfig {
+
+    /**
+     * 设置restTemplate
+     *
+     * @return RestTemplate
+     */
+    @Bean(name = "restTemplate")
+    public RestTemplate createRestTemplate() {
+
+        int oneMinute = 60 * 1000;
+
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(oneMinute);
+        requestFactory.setReadTimeout(oneMinute);
+
+        return new RestTemplate(requestFactory);
+    }
+}
