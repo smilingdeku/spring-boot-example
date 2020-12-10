61ffa5fec70c3678d549d8202e1b79a993f61c52
linzhaoming <linzhaoming@ava.com.cn>
2020-12-10 11:56:48 +0800

    docs: SpringUtil 添加注释

--- src/main/java/org/example/common/util/SpringUtil.java
+++ src/main/java/org/example/common/util/SpringUtil.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.stereotype.Component;
 
+/**
+ * Spring 工具类
+ */
 @Component
 public class SpringUtil implements BeanFactoryPostProcessor {
 
@@ -16,11 +19,23 @@ public class SpringUtil implements BeanFactoryPostProcessor {
         SpringUtil.beanFactory = beanFactory;
     }
 
+    /**
+     * 根据 Bean 名称获取 Bean
+     *
+     * @param name Bean 名称
+     * @return Bean
+     */
     @SuppressWarnings("unchecked")
     public static <T> T getBean(String name) throws BeansException {
         return (T) beanFactory.getBean(name);
     }
 
+    /**
+     * 根据 Bean 类获取 Bean
+     *
+     * @param clz Bean 类
+     * @return Bean
+     */
     public static <T> T getBean(Class<T> clz) throws BeansException {
         return (T) beanFactory.getBean(clz);
     }
