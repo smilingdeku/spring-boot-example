e050e76c41ecd9c734de0492d36de064dd6f70b0
cs12110 <cs12110@163.com>
2020-12-13 00:18:46 +0800

    feat: easyexcel工具类和下载excel功能

---
+++ src/main/java/org/example/common/util/excel/listener/ExcelAnalysisEventListener.java
@@ -0,0 +1,22 @@
+package org.example.common.util.excel.listener;
+
+import com.alibaba.excel.context.AnalysisContext;
+import com.alibaba.excel.event.AnalysisEventListener;
+
+/**
+ * @author walle
+ * @version V1.0
+ * @since 2020-12-13 00:13
+ */
+public class ExcelAnalysisEventListener<T> extends AnalysisEventListener<T> {
+
+    @Override
+    public void invoke(Object data, AnalysisContext context) {
+
+    }
+
+    @Override
+    public void doAfterAllAnalysed(AnalysisContext context) {
+
+    }
+}
